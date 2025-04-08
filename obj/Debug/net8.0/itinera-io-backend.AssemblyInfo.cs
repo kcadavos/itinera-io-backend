20da26fcd9dd6d3ff37dbfccbca382bfddc240b6
espressotoexpressions <keannevillar@gmail.com>
2025-04-07 20:47:37 -0700

    updating controllers and services

--- obj/Debug/net8.0/itinera-io-backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/itinera-io-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("itinera-io-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa635e37058f51586d1a5c787c43172ef4518c1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71c08b9b456f614d54f29b8682e7e29db9180523")]
 [assembly: System.Reflection.AssemblyProductAttribute("itinera-io-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("itinera-io-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
