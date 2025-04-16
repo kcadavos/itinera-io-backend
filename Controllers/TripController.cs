6161c0f61b807816d882c149afd14c02ba66cf76
espressotoexpressions <keannevillar@gmail.com>
2025-04-16 16:25:37 -0700

    updated errors

--- Controllers/TripController.cs
+++ Controllers/TripController.cs
@@ -27,7 +27,7 @@ namespace itinera_io_backend.Controllers
                 return Ok(trips);
             else
             {
-                return BadRequest(new={Message="No trips available"});
+                return BadRequest(new{Message="No trips available"});
             }
         }
 
